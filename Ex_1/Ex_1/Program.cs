ea36a4d3c9fea8e5756e5280d02b7595c22c3e65
NguyenManhThang1447 <90382783+NguyenManhThang1447@users.noreply.github.com>
2021-09-15 23:25:48 +0700

    Ex_1A

---
+++ Ex_1/Ex_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Full name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
