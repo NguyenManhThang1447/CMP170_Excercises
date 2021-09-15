291fb6e4b9d5437308493673b2505e410b22cb42
NguyenManhThang1447 <90382783+NguyenManhThang1447@users.noreply.github.com>
2021-09-15 23:31:19 +0700

    Ex_1B

---
+++ Ex_2/Ex_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input X = ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("Input Y = ");
+            float y = float.Parse(Console.ReadLine());
+            Console.Write("X + Y = ");
+            Console.WriteLine(x + y);
+            Console.Write("X - Y = ");
+            Console.WriteLine(x - y);
+            Console.Write("X * Y = ");
+            Console.WriteLine(x * y);
+            Console.Write("X / Y = ");
+            Console.WriteLine(x / y);
+        }
+    }
+}
